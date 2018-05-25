7b5b2e85ba4ba40515f88e0c0faf8de934cdec52
Viriy <541239223@qq.com>
2018-05-25 11:53:17 +0800

    music  loading  swiper  横评提示

--- js/test.config.js
+++ js/test.config.js
@@ -1,19 +1,23 @@
 (function() {
 
-document.addEventListener("WeixinJSBridgeReady", function() {playmusic.play();}, false);// Apple wx play music; 
+	$(window).on('load',function(){
+		TweenMax.to($("#toploader"),0.5,{autoAlpha:0,delay:.6});
+		playmusic.play();
+	});
+    document.addEventListener("WeixinJSBridgeReady", function() { playmusic.play(); }, false); // Apple wx play music;
+	
+	
 
-function onLoaded() {
-       playmusic.play();//default music play;
-}
+	
+    // 微信分享
+     function ready(){
+       share({
+        title:document.title,
+        desc: '分享描述',
+        link: location.href,
+        imgUrl: '/images/share.jpg',//显示小图标
+        debug: false
+        });
+     }
 
-// 微信分享
-function ready(){
-  share({
-   title:document.title,
-   desc: '分享描述',
-   link: location.href,
-   imgUrl: '/images/share.jpg',//显示小图标
-   debug: false
-   });
-}
 })();
